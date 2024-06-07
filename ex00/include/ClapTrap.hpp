b7525ed9355a5309eaf7e993c8fc7b07276f8604
Julien Houyet <julien.houyet@gmail.com>
2024-06-07 15:58:07 +0200

    feat: start ClapTrap constructor & destructor

--- ex00/include/ClapTrap.hpp
+++ ex00/include/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 13:40:34 by jhouyet           #+#    #+#             */
-/*   Updated: 2024/06/07 14:44:52 by jhouyet          ###   ########.fr       */
+/*   Updated: 2024/06/07 15:42:43 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,26 @@
 #ifndef __CLAPTRAP_H__
 #define __CLAPTRAP_H__
 
+#include <iostream>
+
 class ClapTrap
 {
-	
+	public:
+		ClapTrap( void );
+		ClapTrap( std::string name );
+		ClapTrap( const ClapTrap& copy );
+		ClapTrap& operator=( const ClapTrap& copy );
+		~ClapTrap( void );
+
+		void attack( const std::string& target );
+		void takeDamage( unsigned int amount );
+		void beRepaired( unsigned int amount );
+
+	private:
+		std::string _name;
+		int _hitPoints;
+		int _energyPoint;
+		int _attackDamage;
 };
 
 #endif
--- ex00/src/ClapTrap.cpp
+++ ex00/src/ClapTrap.cpp
@@ -6,8 +6,47 @@
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 13:40:40 by jhouyet           #+#    #+#             */
-/*   Updated: 2024/06/07 13:42:31 by jhouyet          ###   ########.fr       */
+/*   Updated: 2024/06/07 15:57:00 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap( void ) : _name("JohnDoe"), _hitPoints(100), _energyPoint(10), _attackDamage(20)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+ClapTrap::ClapTrap( std::string name ) : _name(name), _hitPoints(100), _energyPoint(10), _attackDamage(20)
+{
+	std::cout << "Constructor called" << std::endl;
+}
+
+ClapTrap::ClapTrap( const ClapTrap& copy ) 
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = copy;
+}
+
+ClapTrap& ClapTrap::operator=( const ClapTrap& copy )
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+    if (this != &copy) {
+        this->_name = copy._name;
+        this->_hitPoints = copy._hitPoints;
+        this->_energyPoint = copy._energyPoint;
+        this->_attackDamage = copy._attackDamage;
+    }
+    return *this;
+}
+
+ClapTrap::~ClapTrap( void )
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+void ClapTrap::attack( const std::string& target ) {}
+
+void ClapTrap::takeDamage( unsigned int amount ) {}
+
+void ClapTrap::beRepaired( unsigned int amount ) {}
